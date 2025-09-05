31c93005633ec212dbbddf16798ca902e9cb155b
Jake Lee <leeshaoherng@hotmail.com>
2025-09-05 17:28:04 +0800

    feat: Messages. Managing Conversation History

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -15,8 +16,12 @@ def main():
         sys.exit(1)
     prompt = sys.argv[1]
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=prompt
+        model="gemini-2.0-flash-001", contents=messages
     )
 
     print(response.text)
