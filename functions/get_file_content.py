94ed2f5c7c7a20cb67bd35f756c6e511c8ea6d4b
Jake Lee <leeshaoherng@hotmail.com>
2025-09-08 15:25:53 +0800

    feat: Adding All Function Declarations

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,6 +1,7 @@
 import os
 
 from config import MAX_CHARS
+from google.genai import types
 
 
 def get_file_content(working_directory, file_path):
@@ -20,3 +21,17 @@ def get_file_content(working_directory, file_path):
         return file_content_string
     except Exception as e:
         return f"Error: Excepton reading file: {e}"
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Gets the contents of the given file as a string, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file, from the working directory.",
+            ),
+        },
+    ),
+)
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,5 +1,7 @@
 import os
 
+from google.genai import types
+
 
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
@@ -21,3 +23,21 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f"Error: Failed to write to file: {file_path}, {e}"
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Overwrites an existing file or writes to a new file if it doesn't exist (and creates required parent dirs safetly), constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The contents to write to the file as a string",
+            ),
+        },
+    ),
+)
